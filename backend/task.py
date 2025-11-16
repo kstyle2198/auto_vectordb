4f8627710028e6d5e22a43d3d293e392bfb13874
jongbaekim <jongbaekim0710@gmail.com>
2025-11-16 11:18:36 +0900

    start 11/16

--- backend/task.py
+++
@@ -1,9 +0,0 @@
-import json
-import redis  # 동기 redis 클라이언트 (Celery 작업자용)
-from .utils.setlogger import setup_logger
-from .utils.config import get_config
-config = get_config()
-logger = setup_logger(f"{__name__}", level=config.LOG_LEVEL)
-
-
-
--- backend/utils/config.py
+++ backend/utils/config.py
@@ -7,7 +7,7 @@ class BaseConfig:
     DEBUG: bool = os.getenv("DEBUG", "False").lower() == "true"
     REDIS_BROKER_URL: str = os.getenv("BROKER_URL", "redis://redis:6379/0")
     REDIS_BACKEND_URL: str = os.getenv("RESULT_BACKEND", "redis://redis:6379/1")
-    REDIS_PUBSUB_URL: str = os.getenv("RESULT_BACKEND", "redis://redis:6379/2")
+    REDIS_PUBSUB_URL: str = os.getenv("REDIS_PUBSUB_URL", "redis://redis:6379/2")
     DATABASE_URL: str = os.getenv("DATABASE_URL", "")
     OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY", "")
     LOG_LEVEL: str = os.getenv("LOG_LEVEL", "INFO")
